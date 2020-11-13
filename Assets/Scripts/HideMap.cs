278aacfc2bf8c9932b66910b593b69902b6cb73b
aleDanna <alessio.danna.94@gmail.com>
2020-11-13 19:03:34 +0000

    first commit

---
+++ Assets/Scripts/HideMap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideMap : MonoBehaviour {
+    // Start is called before the first frame update
+
+    private GameObject camera;
+    private bool hide;
+    void Start()
+    {
+        this.hide = true;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetButtonDown("Map") == true) {
+            hide = !hide;
+        }
+        this.gameObject.transform.Find("minimap").gameObject.SetActive(!hide);
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float playerSpeed = 8;
+    private GameObject astronaut;
+    private Rigidbody rigidBody;
+
+    private bool isMoving;
+
+    private void Start() {
+        astronaut = this.gameObject.transform.Find("astronaut").gameObject;
+        rigidBody = this.gameObject.GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        float horizontalAxis = Input.GetAxisRaw("Horizontal");
+        float verticalAxis = Input.GetAxisRaw("Vertical");
+        
+        //assuming we only using the single camera:
+        var camera = Camera.main;
+ 
+        //camera forward and right vectors:
+        var forward = camera.transform.forward;
+        var right = camera.transform.right;
+ 
+        //project forward and right vectors on the horizontal plane (y = 0)
+        forward.y = 0f;
+        right.y = 0f;
+        forward.Normalize();
+        right.Normalize();
+
+        var rotation = camera.transform.rotation;
+        rotation.x = 0;
+        rotation.z = 0;
+        astronaut.transform.rotation = rotation;
+ 
+        //this is the direction in the world space we want to move:
+        var desiredMoveDirection = forward * verticalAxis + right * horizontalAxis;
+
+        //now we can apply the movement:
+        rigidBody.velocity = desiredMoveDirection * playerSpeed * Time.deltaTime * 100;
+        isMoving = rigidBody.velocity.x != 0 || rigidBody.velocity.z != 0;
+    }
+
+    void Update()
+    {
+        astronaut.GetComponent<Animator>().SetBool("Moving", isMoving);
+    }
+}
